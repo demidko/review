8cffe3836bcc5c76e2fb924105f10de2cae079e4
Daniil Demidko <demidko@farpost.com>
2021-03-12 10:58:57 +1000

    skeleton

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,28 +1,16 @@
-import com.github.ajalt.clikt.core.CliktCommand
-import io.ktor.application.*
-import io.ktor.http.HttpStatusCode.Companion.OK
-import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) = object : CliktCommand("Application description here") {
+fun main(args: Array<String>) {
 
-  override fun run() {
-    /**
-     * Your code here
-     */
-    mockHealthyChecks()
-  }
+  val token = args.first()
 
-  fun mockHealthyChecks() {
-    echo("localhost:80 -> HTTP 200 OK")
-    val server = embeddedServer(Netty) {
-      routing {
-        get("/") { call.respond(OK) }
+  embeddedServer(Netty) {
+    routing {
+      post("/merge-request") {
+
       }
     }
-    server.start(false)
-  }
-
-}.main(args)
+  }.start(true)
+}
