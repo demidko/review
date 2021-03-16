b335bec4f8bca23b52a525a28977d1d85cc82a2e
Daniil Demidko <demidko@farpost.com>
2021-03-16 17:23:51 +1000

    java parser

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 }
 
 dependencies {
+  implementation("com.github.javaparser:javaparser-core:3.20.0")
   implementation("ch.qos.logback:logback-classic:1.3.0-alpha5")
   implementation("io.ktor:ktor-gson:1.5.2")
   implementation("io.ktor:ktor-server-netty:1.5.2")
