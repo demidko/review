cbe7e2abd287e7fb929c832001739d13a36f2749
Daniil Demidko <demidko@farpost.com>
2021-03-17 11:38:52 +1000

    new printer comfiguration (javaparser-core)

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN --mount=type=cache,target=./.gradle gradle clean test shadowJar
 FROM openjdk:15 as backend
 WORKDIR /root
 COPY --from=builder /project/*.jar ./app
-ENV GITLAB_URL=gitlab.example.com
-ENV GITLAB_TOKEN=example_secret_token
+ENV GITLAB_URL=""
+ENV GITLAB_TOKEN=""
 ENTRYPOINT "java" "-jar" "/root/app" $GITLAB_URL $GITLAB_TOKEN
---
+++ src/main/kotlin/Parser.kt
@@ -0,0 +1,106 @@
+import com.github.javaparser.JavaParser
+import com.github.javaparser.ParserConfiguration
+import com.github.javaparser.ParserConfiguration.LanguageLevel.JAVA_16_PREVIEW
+import com.github.javaparser.ast.ImportDeclaration
+import com.github.javaparser.ast.NodeList
+import com.github.javaparser.ast.PackageDeclaration
+import com.github.javaparser.ast.body.*
+import com.github.javaparser.ast.expr.MarkerAnnotationExpr
+import com.github.javaparser.ast.expr.NormalAnnotationExpr
+import com.github.javaparser.ast.expr.SingleMemberAnnotationExpr
+import com.github.javaparser.ast.nodeTypes.NodeWithSimpleName
+import com.github.javaparser.ast.stmt.BlockStmt
+import com.github.javaparser.printer.DefaultPrettyPrinterVisitor
+import com.github.javaparser.printer.configuration.DefaultConfigurationOption
+import com.github.javaparser.printer.configuration.DefaultPrinterConfiguration
+import com.github.javaparser.printer.configuration.DefaultPrinterConfiguration.ConfigOption.INDENTATION
+import com.github.javaparser.printer.configuration.DefaultPrinterConfiguration.ConfigOption.PRINT_COMMENTS
+import com.github.javaparser.printer.configuration.Indentation
+import com.github.javaparser.printer.configuration.Indentation.IndentType.SPACES
+import kotlin.text.RegexOption.MULTILINE
+
+private val javaParser = ParserConfiguration()
+  .apply { languageLevel = JAVA_16_PREVIEW }
+  .let(::JavaParser)
+
+fun String.parseJavaArchitecture(): String {
+  val visitor = JavaDeclarationVisitor()
+  javaParser
+    .parse(this)
+    .result.get()
+    .accept(visitor, null)
+  return visitor.toString()
+}
+
+private class JavaDeclarationVisitor : DefaultPrettyPrinterVisitor(PrettyPrinterConfiguration) {
+
+  private var isInterface = false
+
+  private val blanks = "^(?:[\\t ]*(?:\\r?\\n|\\r))+".toRegex(MULTILINE)
+
+  override fun visit(n: ClassOrInterfaceDeclaration, arg: Void?) {
+    isInterface = n.isInterface
+    super.visit(OrderedClassOrInterfaceDeclaration(n), arg)
+  }
+
+  override fun visit(n: PackageDeclaration, arg: Void?) {}
+
+  override fun visit(n: ImportDeclaration, arg: Void?) {}
+
+  override fun visit(n: MarkerAnnotationExpr, arg: Void?) {}
+
+  override fun visit(n: SingleMemberAnnotationExpr, arg: Void?) {}
+
+  override fun visit(n: NormalAnnotationExpr, arg: Void?) {}
+
+  override fun visit(n: AnnotationDeclaration?, arg: Void?) {}
+
+  override fun visit(n: AnnotationMemberDeclaration?, arg: Void?) {}
+
+  override fun visit(n: InitializerDeclaration?, arg: Void?) {}
+
+  override fun visit(n: ConstructorDeclaration, arg: Void?) {
+    if (n.isPublic) super.visit(n, arg)
+  }
+
+  override fun visit(n: EnumDeclaration, arg: Void?) {
+    if (n.isPublic) super.visit(n, arg)
+  }
+
+  override fun visit(n: MethodDeclaration, arg: Void?) {
+    if (n.isPublic || isInterface) super.visit(n, arg)
+  }
+
+  override fun visit(n: BlockStmt, arg: Void?) {
+    printer.print(";")
+  }
+
+  override fun visit(n: FieldDeclaration, arg: Void?) {
+    if (n.isPublic) super.visit(n, arg)
+  }
+
+  override fun toString() = super.toString()
+    .replace(blanks, String())
+    .replace(" ;", ";")
+    .trim()
+}
+
+private val PrettyPrinterConfiguration = DefaultPrinterConfiguration().apply {
+  addOption(DefaultConfigurationOption(PRINT_COMMENTS))
+  addOption(DefaultConfigurationOption(INDENTATION, Indentation(SPACES, 2)))
+}
+
+private class OrderedClassOrInterfaceDeclaration(d: ClassOrInterfaceDeclaration) :
+  ClassOrInterfaceDeclaration(
+    d.tokenRange.orElse(null), d.modifiers, d.annotations, d.isInterface,
+    d.name, d.typeParameters, d.extendedTypes, d.implementedTypes, d.members
+  ) {
+  override fun getMembers() = NodeList(super.getMembers().sortedBy(BodyDeclaration<*>::name))
+}
+
+private val BodyDeclaration<*>.name
+  get() = when (this) {
+    is FieldDeclaration -> variables?.getOrNull(0)?.nameAsString ?: String()
+    is NodeWithSimpleName<*> -> nameAsString
+    else -> String()
+  }
