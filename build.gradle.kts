9b620544cd4191f1737fe383a5a285a8e994dda2
Daniil Demidko <demidko@farpost.com>
2021-03-19 10:00:14 +1000

    logback stable

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 dependencies {
   implementation("io.sentry:sentry-logback:4.3.0")
   implementation("com.github.javaparser:javaparser-core:3.20.0")
-  implementation("ch.qos.logback:logback-classic:1.3.0-alpha5")
+  implementation("ch.qos.logback:logback-classic:1.2.3")
   implementation("io.ktor:ktor-gson:1.5.2")
   implementation("io.ktor:ktor-server-netty:1.5.2")
   implementation("io.ktor:ktor-client-cio:1.5.2")
