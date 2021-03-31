264ddee7be471e16996f642863d2add116e083cc
Daniil Demidko <demidko@farpost.com>
2021-03-31 10:38:55 +1000

    Рефакторинг, минорные улучшения

--- src/main/kotlin/Diff.kt
+++ src/main/kotlin/Diff.kt
@@ -1,38 +1,60 @@
 import org.gitlab4j.api.RepositoryFileApi
 import org.gitlab4j.api.models.Diff
 import org.gitlab4j.api.models.MergeRequest
-import java.io.File
+import java.io.File.createTempFile
+import java.nio.file.Files.createTempDirectory
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.Path
+import kotlin.io.path.div
+import kotlin.io.path.nameWithoutExtension
 
 
-
+@ExperimentalPathApi
 fun Diff.architectureDiff(api: RepositoryFileApi, mergeRequest: MergeRequest) {
 
-  val before = when {
+  val sourceDirectory = createTempDirectory(
+    "${mergeRequest.sourceProjectId}_${mergeRequest.sourceBranch}"
+  )
+
+  val targetDirectory = createTempDirectory(
+    "${mergeRequest.targetProjectId}_${mergeRequest.targetBranch}"
+  )
+
+  val oldName = Path(oldPath).nameWithoutExtension
+
+  val newName = Path(newPath).nameWithoutExtension
+
+  sourceDirectory / "df"
+
+  val oldContent = when {
     newFile -> String()
     else -> api
-      .getFile(projId, oldPath, mergeRequest.targetBranch)
+      .getFile(mergeRequest.projectId, oldPath, mergeRequest.targetBranch)
       .decodedContentAsString
       .parseJavaArchitecture()
   }
-  val after = when {
+
+  val newContent = when {
     deletedFile -> String()
     else -> api
-      .getFile(projId, it.newPath, mergeRequest.sourceBranch)
+      .getFile(mergeRequest.projectId, newPath, mergeRequest.sourceBranch)
       .decodedContentAsString
       .parseJavaArchitecture()
   }
-  diff(before, after)
+
+
+
+  diff(oldContent, newContent)
 }
 
-fun diff(before: String, after: String): String {
+fun diff(beforeName: String, beforeContent: String, afterName: String, afterContent: String): String {
 
-  val old = File.createTempFile("before", null).apply { writeText(before) }
+  val old = createTempFile(beforeName, ".java").apply { writeText(beforeContent) }
 
-  val new = File.createTempFile("after", null).apply { writeText(after) }
+  val new = createTempFile(afterName, ".java").apply { writeText(afterContent) }
 
   return "git diff --no-index $old $new"
     .shell()
     .split("\n")
-    .drop(5)
     .joinToString("\n")
 }
